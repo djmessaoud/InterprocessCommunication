5485ec70389a399064ba0fcbfe033bab2a0418b5
djmessaoud <djardi.messaoud@gmail.com>
2021-11-05 07:21:38 +0300

    Added project

---
+++ ConsoleClient1/ConsoleClient1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Pipes;
+using System.IO;
+using System.Threading;
+namespace ConsoleClient1
+{
+    class Program
+    {
+      public static  NamedPipeClientStream pipe;
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            pipe = new NamedPipeClientStream(".", "Intern", PipeDirection.InOut);
+                pipe.Connect();
+                
+                if (pipe.IsConnected)
+                {
+                    Console.WriteLine("Connected to server");
+                    string msg = "";
+                    Console.Write("Enter Statement in format [A op B] e.g [{0}*{1}] : ",r.Next(0,999).ToString(),r.Next(1,999).ToString());
+                    msg = Console.ReadLine();
+                    pipe.Write(Encoding.UTF8.GetBytes(msg),0,msg.Length);
+                    Thread GetResult = new Thread(ResultShow);
+                    GetResult.Start();
+                }
+                Console.Read();
+            
+        }
+        private static void ResultShow()
+        {
+            while (pipe.IsConnected)
+            {
+                byte[] buffer = new byte[1024];
+                pipe.Read(buffer, 0, buffer.Length);
+                Console.WriteLine(" Result from server = " + Encoding.UTF8.GetString(buffer));
+            }
+        }
+    }
+}
---
+++ ConsoleServer1/ConsoleServer1/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Pipes;
+using System.IO;
+namespace ConsoleServer1
+{
+    class Program
+    {
+        private static double doTheMath(string Data) //Function to take string from pipe and do the math :D
+        {
+            int n1 = 0, n2 = 0;
+            char op = 'Q';
+            string tempUse = "";
+            for (int i = 0; i < Data.Length; i++) // take first number into n1
+            {
+                if ((int)Data[i] >= 48 && (int)Data[i] <= 57) tempUse += Data[i];
+                else // take operand
+                {
+                    Console.WriteLine("Number 1 : " + tempUse);
+                    op = Data[i];
+                    n1 = int.Parse(tempUse);
+                    tempUse = "";
+                }
+            }
+            Console.WriteLine("Operad : " + op);
+            Console.WriteLine("Number 2 : " + tempUse);
+            n2 = int.Parse(tempUse); //n2 will stay alone so we take it
+            switch (op) // do the math
+            {
+                case '+': { return n1 + n2; break; }
+                case '-': { return n1 - n2; break; }
+                case '*': { return n1 * n2; break; }
+                case '/': { return n1 / n2; break; }
+            }
+            return 0;
+        }
+    
+    static void Main(string[] args)
+        {
+            NamedPipeServerStream pipe = new NamedPipeServerStream("Intern", PipeDirection.InOut); //NamedPipeLine
+
+            pipe.WaitForConnection(); 
+            Console.WriteLine("Client Connected ! "); //Declaring 
+            byte[] bufferr = new byte[1024]; 
+            string MessageFromClient = ""; // MessageFromClient, we will use it to filter from byte array only real chars.
+            int k = 0;
+            while (pipe.IsConnected)
+            {
+                MessageFromClient = "";
+                pipe.Read(bufferr, 0, bufferr.Length); //Read from client
+                while (bufferr[k]!='\0') //Filtring real chars from the rest initialized chars in byte array [1024]
+                {       
+                    MessageFromClient += (char)bufferr[k];
+                    k++;
+                }   
+                double result = 0;
+                if (MessageFromClient != "") //If the message is not empty, do the math and send the result back to client.
+                {
+                    result = doTheMath(MessageFromClient);
+                    Console.WriteLine("Result =  " + result);
+                    byte[] resultBuffer = Encoding.UTF8.GetBytes(result.ToString());
+                    pipe.Write(resultBuffer, 0, resultBuffer.Length);
+                }
+                MessageFromClient = "";
+                k = 0;
+            }
+            Console.Read();
+        }
+
+    }
+
+}
